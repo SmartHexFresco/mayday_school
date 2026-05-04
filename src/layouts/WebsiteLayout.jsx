fce259060a143c9eee0e45cc0f6e768832462595
kizito <kizitoomene120@gmail.com>
2026-05-04 18:45:44 +0100

    updated something

--- src/layouts/WebsiteLayout.jsx
+++ src/layouts/WebsiteLayout.jsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router-dom'
 import Navbar from '@components/website/Navbar'
 import Footer from '@components/website/Footer'
 import About from  '@pages/website/About'
-import WhatsAppButton from '@components/WhatsAppButton'
+import WhatsAppButton from '@components/dashboard/WhatsAppButton'
 
 const WebsiteLayout = () => {
   return (
